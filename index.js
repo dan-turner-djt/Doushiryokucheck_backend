e083d8eabf1736a2e33c86e55ea1f66bfbbb11ff
Dan Turner <dan.turner.djt@gmal.com>
2023-10-07 20:18:40 +0100

    port 5000

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import express from 'express'
-import dotenv from 'dotenv'
-
-dotenv.config();
 
 const app = express();
-const port = process.env.port;
+const port = 5000;
 
-app.listen(5000, () => console.log(`Server is running on ${port}`));
+app.listen(port, () => console.log(`Server is running on ${port}`));
 
 app.get('/', (req, res) => res.json("Hello Mr Khin"));
