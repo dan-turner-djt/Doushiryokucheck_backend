3f6111e32b42ef0718562a2d29e21d5057f30e90
Dan Turner <dan.turner.djt@gmal.com>
2023-10-08 00:31:55 +0100

    add cors

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express, { Express, Request, Response } from 'express'
+import cors from 'cors'
 import { QuestionInfo } from './defs';
 
 const app: Express = express();
+app.use(cors());
 const port = 5000;
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
