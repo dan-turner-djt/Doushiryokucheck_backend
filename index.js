7e56e1bf09213045bd179602bc92fee65b3b7f8e
Dan Turner <dan.turner.djt@gmal.com>
2023-10-07 19:35:18 +0100

    update test

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.listen(5000, () => console.log('Api running on port 5000'));
 
-app.get('/', (req, res) => res.json("It's running"));
+app.get('/', (req, res) => res.json("Hello Mr Khin"));
