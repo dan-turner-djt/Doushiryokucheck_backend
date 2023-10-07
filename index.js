b2a996b3476f97088c78ddab349c9f3c64b9ba61
Dan Turner <dan.turner.djt@gmal.com>
2023-10-07 20:15:00 +0100

    show port dynamically

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 
 const app = express();
+const port = process.env.port;
 
-app.listen(5000, () => console.log('Api running on port 5000'));
+app.listen(5000, () => console.log(`Server is running on ${port}`));
 
 app.get('/', (req, res) => res.json("Hello Mr Khin"));
