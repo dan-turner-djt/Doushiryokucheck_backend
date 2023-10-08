5ac3e3c72f10e596333a235fc8ac3bbc0bebd1a8
Dan Turner <dan.turner.djt@gmal.com>
2023-10-08 21:24:26 +0100

    add live checkability

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ import https from 'https';
 import path from 'path';
 import fs from 'fs';
 
+const isLive = true;
+
 const app: Express = express();
 app.use(cors());
 const port = 5000;
@@ -19,6 +21,8 @@ sslServer.listen(port, () => console.log(`Secure server is running on ${port}`))
 //app.listen(port, () => console.log(`Server is running on ${port}`));
 
 app.get('/', (req: Request, res: Response) => res.json("Successful request"));
+
+app.get('/checkLive', (req: Request, res: Response) => res.json({isLive: isLive}));
 
 app.get('/question', (req: Request, res: Response) => {
 
