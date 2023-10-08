d65f4f99936c2722dda9dd72f3b3a35e2a6f7708
Dan Turner <dan.turner.djt@gmal.com>
2023-10-08 02:43:29 +0100

    switch to secure server

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
 import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
 import { QuestionInfo } from './defs';
+import https from 'https';
+import path from 'path';
+import fs from 'fs';
 
 const app: Express = express();
 app.use(cors());
 const port = 5000;
 
-app.listen(port, () => console.log(`Server is running on ${port}`));
+const sslServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, '..', 'cert', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, '..', 'cert', 'cert.pem'))
+}, app);
+
+sslServer.listen(port, () => console.log(`Secure server is running on ${port}`))
 
 app.get('/', (req: Request, res: Response) => res.json("Successful request"));
 
